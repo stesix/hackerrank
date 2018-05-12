a245ebdff74318ae79d3e45b38ab51ea02de8b67
stesix <stefano.sicco@gmail.com>
2018-05-12 11:20:47 +0200

    Remove some ESLint rules
    Too many warnings for stuff unrelated to my code

--- AlternatingCharacters/run.js
+++ AlternatingCharacters/run.js
@@ -5,11 +5,11 @@ var input_stdin = "";
 var input_stdin_array = "";
 var input_currentline = 0;
 
-process.stdin.on('data', function (data) {
+process.stdin.on('data', function(data) {
     input_stdin += data;
 });
 
-process.stdin.on('end', function () {
+process.stdin.on('end', function() {
     input_stdin_array = input_stdin.split("\n");
     main();
 });
@@ -25,11 +25,11 @@ function alternatingCharacters(s) {
 
     let lastEval = s.charAt(0);
     for (let i = 1; i < s.length; i++) {
-        if (s.charAt(i) !== lastEval) {
+        if (s.charAt(i) !== lastEval) 
             lastEval = s.charAt(i);
-        } else {
+        else 
             del++;
-        }
+        
     }
 
     return del;
@@ -37,7 +37,7 @@ function alternatingCharacters(s) {
 
 function main() {
     var q = parseInt(readLine());
-    for(var a0 = 0; a0 < q; a0++){
+    for (var a0 = 0; a0 < q; a0++){
         var s = readLine();
         var result = alternatingCharacters(s);
         process.stdout.write("" + result + "\n");
